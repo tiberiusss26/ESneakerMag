3d5a50fbdfe4d1ff1f00d9f21960f7713786efb4
tiberiusss26 <titirigatibi26@gmail.com>
2023-01-14 16:42:15 +0200

    purchase/new-columns

--- proiect/Models/Purchase.cs
+++ proiect/Models/Purchase.cs
@@ -3,6 +3,9 @@ namespace proiect.Models
 {
 	public class Purchase
 	{
+		public DateTime PurchaseDate { get; set; }
+		public double FinalPrice { get; set; }
+
 		public Guid OrderId { get; set; }
 		public Order Order { get; set; }
 
